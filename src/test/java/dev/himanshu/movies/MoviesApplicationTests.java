792afc79cc9ac633e0e4b72c81aa9cc79ae3f305
Himanshu Abburu <himanshuabburu@gmail.com>
2024-02-06 21:08:06 +0000

    initial commit

---
+++ src/test/java/dev/himanshu/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.himanshu.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
